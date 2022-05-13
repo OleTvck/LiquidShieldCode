ea9f9ad9b91972492e3c8c27333c87ae3d9392b7
Tucker <oletvck@gmail.com>
2022-05-13 13:30:46 -0500

    Added Functionality and IDs
    Added some styling and unique ID's to all descriptions. This allows for the buttons to work on each individual description.

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,13 @@
     ref
   } from "vue";
   import axios from "axios"
-  
+
   const result = ref({})
-  
+
   const callAPI = () => {
     const userInput = document.getElementById("userInput").value
     axios
-      .get('https://services.nvd.nist.gov/rest/json/cves/1.0?keyword='+userInput, {
+      .get('https://services.nvd.nist.gov/rest/json/cves/1.0?keyword=' + userInput, {
         timeout: 30000
 
       })
@@ -20,24 +20,38 @@
         console.log(result.value)
       })
       .catch(err => console.error(err));
-      console.log(userInput)
-      
+    console.log(userInput)
+
   }
 </script>
 
-<template>
+<template class="row">
   <div>
+    <h1 class="display-3 text-center">Type what you would <br/>like to search the API for.</h1>
+  </div>
+  <div class="col text-center">
     <input type="string" id="userInput">
-    <button @click="callAPI">Call API</button>
+    <button @click="callAPI" class="btn btn-primary ">Call API</button>
 
-    <div v-for="cve, index in result.CVE_Items" :key="`ID-${index}`">
+    <div class="" v-for="cve, index in result.CVE_Items" :key="`ID-${index}`">
       <br />
       <div>ID: {{ cve.cve.CVE_data_meta.ID }}</div>
-      <!--<div>Impact Score: {{ cve.impact.baseMetricV2.impactScore }}</div>-->
-      <br />
-      <div>Description: {{ cve.cve.description.description_data[0].value }}</div>
+      <div>Impact Score: {{ cve.impact.baseMetricV2 ? cve.impact.baseMetricV2.impactScore : `no impact` }}</div>
+      <!-- Add Collapse Button Here  -->
+      <p>
+        <button class="btn btn-primary" type="button" data-bs-toggle="collapse" :data-bs-target="`#ID-${index}`"
 
-
+          aria-expanded="false" aria-controls="collapseWidthExample">
+          Toggle width collapse
+        </button>
+      </p>
+      <!-- Collapse this Description -->
+      <div style="min-height: 120px;" class="text-align-center">
+        <div class="collapse collapse-horizontal mx-auto" :id="`ID-${index}`">
+          <div class="card card-body mx-auto" style="width: 300px;">
+            Description: {{ cve.cve.description.description_data[0].value }} </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
